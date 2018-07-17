1d54d83b17834a56dc718bcb744311e4c6a30eb6
Dan Taylor <qubitron@users.noreply.github.com>
2018-07-16 21:37:39 -0700

    Removing docker files

--- app/main.py
+++ app/main.py
@@ -1,17 +1,5 @@
 from flask import Flask, render_template
-import ptvsd
-
-# Allow other computers to attach to ptvsd at this IP address and port, using the secret
-ptvsd.enable_attach("shhh", address = ('0.0.0.0', 4001))
-
-# Pause the program until a remote debugger is attached
-ptvsd.wait_for_attach()
-
 app = Flask(__name__)
-
-import os
-print(os.getcwd())
-print("Starting up!")
 
 @app.route('/')
 def index():
