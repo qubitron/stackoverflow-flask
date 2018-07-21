b654111caf217503cc8fcfbbb2d6e967ea5cfaf3
Dan Taylor <qubitron@users.noreply.github.com>
2018-07-21 23:21:51 +0100

    Cleanup

--- app/main.py
+++ app/main.py
@@ -3,12 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  print("Hello!")
   return app.send_static_file('index.html')
 
 @app.route('/api/data')
 def get_data():
-  print("Data!")
   return app.send_static_file('data.json')
 
 if __name__ == '__main__':
